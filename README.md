23e91c0aa9a8075e8fa8c85b68dcf1cc8a345a3e
ndhpro <ndhpro.psa@gmail.com>
2020-08-19 14:35:17 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Advanced PSI-walk
---
+++ main.py
@@ -0,0 +1,120 @@
+import sys
+import time
+from copy import copy, deepcopy
+import networkx as nx
+import numpy as np
+import pandas as pd
+from env import Environment
+from agent import QLearningTable
+from pathlib import Path
+
+
+def load_graph(path):
+    G = nx.MultiDiGraph()
+    with open(path, 'r') as f:
+        lines = f.readlines()
+
+    for line in lines[2:]:
+        line = line.strip()
+        if ' ' in line and line.find(' ') == line.rfind(' '):
+            (u, v) = line.split(' ')
+            G.add_edge(u, v)
+
+    # Add super_root
+    child = set()
+    for u, v in G.edges():
+        if u != v:
+            child.add(v)
+    start_edge = list()
+    for n in G.nodes():
+        if len(child) == len(G.nodes) or n not in child:
+            start_edge.append(('ndhpro', n))
+
+    G.add_edges_from(start_edge)
+
+    return G
+
+
+def update(num_episode, env, RL, steps, all_costs):
+    for e in range(num_episode):
+        i = 0
+        cost = 0
+        done = False
+        state = env.reset()
+
+        while not done:
+            avail_action = env.get_avail_action(state)
+            action = RL.choose_action(str(state), avail_action)
+
+            state_, reward, done = env.step(action, state)
+            table_id = 0 if np.random.uniform() > 0.5 else 1
+            cost += RL.learn(str(state), action,
+                             reward, str(state_), table_id)
+
+            state = deepcopy(state_)
+            # print(state['node'], end=' ')
+
+            i += 1
+            if i == 50:
+                break
+        all_costs.append(cost)
+        steps.append(i)
+        # print()
+
+        if (e+1) % (num_episode//9) == 0:
+            RL.reduce_epsilon()
+
+
+def get_final_path(path, env, RL, steps, all_costs):
+    f_path = Path('results/') / (Path(path).stem + '.txt')
+    Q = [RL.get_q_table(i) for i in range(2)]
+    # for i in range(2):
+    #     print(Q[i].head())
+    state = env.reset()
+    i = 0
+    done = 0
+    with open(f_path, 'w') as f:
+        while not done and i < 50:
+            avail_actions = env.get_avail_action(state)
+            action = list()
+            try:
+                state_action = [
+                    Q[i].loc[str(state), avail_actions] for i in range(2)]
+            except Exception as e:
+                print(e)
+                break
+            action = [state_action[i].idxmax() for i in range(2)]
+            action = action[0] if Q[0].loc[str(state), action[0]] >= Q[1].loc[
+                str(state), action[1]] else action[1]
+            state, _, done = env.step(action, state)
+            f.write(str(state['node']) + '\n')
+            if (state['node'], state['node']) in env.graph.edges():
+                f.write(str(state['node']) + '\n')
+            i += 1
+
+    # RL.plot_results(steps, all_costs)
+
+
+# Main
+def run_file(path):
+    print(path)
+    G = load_graph(path)
+    if len(G.edges()) == 0:
+        print('Graph has no edge')
+        return
+    # print(len(G.nodes()), len(G.edges()), end=' ')
+    env = Environment(graph=G, root='ndhpro')
+    RL = QLearningTable(actions=G.nodes())
+    steps = []
+    all_costs = []
+
+    t = time.time()
+    n_epoch = 1000
+    update(n_epoch, env, RL, steps, all_costs)
+    # print(round((time.time()-t), 2))
+
+    get_final_path(path, env, RL, steps, all_costs)
+
+
+if __name__ == "__main__":
+    run_file(sys.argv[1])
